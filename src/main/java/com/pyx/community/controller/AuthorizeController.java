aea867914d0092dbedeb719daf4e8135361ffc8e
uuzpid <841856330@qq.com>
2020-06-24 18:24:48 +0800

    third

--- src/main/java/com/pyx/community/controller/AuthorizeController.java
+++ src/main/java/com/pyx/community/controller/AuthorizeController.java
@@ -4,29 +4,49 @@ import com.pyx.community.dto.AccessTokenDTO;
 import com.pyx.community.dto.GithubUser;
 import com.pyx.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
-                           @RequestParam(name = "state")String state) {
+                           @RequestParam(name = "state")String state,
+                           HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("c3a4af36d919a94858b4");
-        accessTokenDTO.setClient_secret("cc027a4267e09ec9cba50428cc82ac5b7c160707");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser githubUser = githubProvider.githubUser(accessToken);
-        System.out.println(githubUser.getName());
-        System.out.println(githubUser.getId());
-        return "index";
+        if(githubUser!=null){
+            //登录成功 写cookie和session
+            request.getSession().setAttribute("user",githubUser);
+            //重定向跳转，可以只跳转到localhost:8887
+            //不再转到http://localhost:8887/callback?code=50c1c742d78e0771f392&state=1
+            return "redirect:/";
+        }else {
+            //登录失败，重新登录
+            return "redirect:/";
+        }
     }
 }
